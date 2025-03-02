98edd761874460c30f47a2e401234dce39aea07f
sdogantekin <serkan.dogantekin@gmail.com>
2025-03-02 14:15:34 +0100

    Initial Commit

---
+++ TinySolarSystem/TinySolarSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinySolarSystemApp.swift
+//  TinySolarSystem
+//
+//  Created by Serkan Dogantekin on 2.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct TinySolarSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TinySolarSystemTests/TinySolarSystemTests.swift
@@ -0,0 +1,17 @@
+//
+//  TinySolarSystemTests.swift
+//  TinySolarSystemTests
+//
+//  Created by Serkan Dogantekin on 2.03.2025.
+//
+
+import Testing
+@testable import TinySolarSystem
+
+struct TinySolarSystemTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
